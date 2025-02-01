7eabc8b141851ed99d5dcc392c01039bf2f21622
redecki <michal.redecki@coraltravel.pl>
2025-02-01 21:55:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".swiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: "auto",
+    slidesPerView: 5,
     coverflowEffect: {
         rotate: 0,
         stretch: 0,
